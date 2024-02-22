da7956527119e4022f1ae06fa627e35b0aec7ff9
jiyunyu <jiyunyu@ezensys.co.kr>
2024-02-22 14:18:58 +0900

    Posts Contetnt done!

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 
 import Counter from './features/counter/Counter';
+import PostsList from './features/posts/PostsList';
+import AddPostForm from './features/posts/AddPostForm';
 
 function App() {
   return (
     <div className="App">
-      <Counter />
+      {/* <Counter /> */} 
+      <AddPostForm />
+      <PostsList />
     </div>
   );
 }
